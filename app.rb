919a419e554040662a31325002dc1330e84c95ff
RyanSnodgrass <res0428@yahoo.com>
2014-04-18 12:26:18 -0400

    added cover page with background image

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require "sinatra/activerecord"
 #RACK_ENV=production rackup config.ru
 set :database, "sqlite3:///sinatra_four_hw_app.db"
 
+get "/" do 
+	erb :"home"
+end
+
 class Professor <ActiveRecord::Base
 end
 
