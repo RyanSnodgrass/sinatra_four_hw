29e4d24e44c8809360f6b558a70ede627abc4c11
RyanSnodgrass <res0428@yahoo.com>
2014-04-18 17:40:34 -0400

    added show page for professors

--- app.rb
+++ app.rb
@@ -23,17 +23,17 @@ get "/subjects/index.erb" do
  erb :"/subjects/index"
 end
 
-get "/courses/:id/show.erb/" do
+get "/courses/:id/show.erb" do
 	@course_to_show = Course.find(params[:id])
 	erb :"courses/show"
 end
 
-get "/professors/:id/show.erb/" do
+get "/professors/:id/show.erb" do
 	@professor_to_show = Professor.find(params[:id])
-	erb :"Professor/show"
+	erb :"professors/show"
 end
 
-get "/subjects/:id/show.erb/" do
+get "/subjects/:id/show.erb" do
 	@subject_to_show = Subject.find(params[:id])
 	erb :"subjects/show"
 end
