202325daaba1aa6686a778b4f346bc2c4683fcac
RyanSnodgrass <res0428@yahoo.com>
2014-04-18 13:09:19 -0400

    added nav bar to all index pages

--- app.rb
+++ app.rb
@@ -7,6 +7,18 @@ set :database, "sqlite3:///sinatra_four_hw_app.db"
 get "/" do 
 	erb :"home"
 end
+		# "/courses/index"
+get "/courses/index.erb" do
+ erb :"/courses/index"
+end
+
+get "/professors/index.erb" do
+ erb :"/professors/index"
+end
+
+get "/subjects/index.erb" do
+ erb :"/subjects/index"
+end
 
 class Professor <ActiveRecord::Base
 end
